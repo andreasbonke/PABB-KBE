52062572130361b5cb283fe97cd2969937866d78
AndreasBonke <s0563482@htw-berlin.de>
2020-04-22 17:38:40 +0200

    Update RunMeRunnerTest.java

--- runmerunner/src/test/java/htwb/ai/PABB/RunMeRunnerTest.java
+++ runmerunner/src/test/java/htwb/ai/PABB/RunMeRunnerTest.java
@@ -5,10 +5,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RunMeRunnerTest {
+    private RunMeRunner rmr;
 
     @Test
-    void correctOutput(){
-        RunMeRunner rmr = new RunMeRunner("htwb.ai.TestClass");
+    void correctClassNameTest(){
+        rmr = new RunMeRunner("htwb.ai.TestClass");
+        //rmr = new RunMeRunner("java.lang.Number");
+        rmr.checkTheClass();
+    }
+
+    @Test
+    void interfaceClassTest(){
+        rmr = new RunMeRunner("java.io.Closeable");
+        rmr.checkTheClass();
+    }
+
+    @Test
+    void abstractClassTest(){
+        rmr = new RunMeRunner("java.lang.reflect.Executable");
+        rmr.checkTheClass();
+    }
+
+    @Test
+    void dontExistClassTest(){
+        rmr = new RunMeRunner("htw.ai.FailTestClass");
+        rmr.checkTheClass();
+    }
+
+    @Test
+    void nullClassNameTest(){
+        rmr = new RunMeRunner(null);
         rmr.checkTheClass();
     }
 
