faa46434471772a59410ac6c4bd7c1785c5440df
Peter <s0563842@htw-berlin.de>
2020-05-06 18:38:13 +0200

    finished.

--- songsservlet/src/main/java/htwb/ai/PABB/controller/SongServlet.java
+++ songsservlet/src/main/java/htwb/ai/PABB/controller/SongServlet.java
@@ -24,45 +24,42 @@ public class SongServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    //private String uriToDB = null;
-    private String welcome;
-
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        welcome = "Welcome";
-    }
-
-    //@Override
-    public void doGet2(HttpServletRequest request,
-                       HttpServletResponse response) throws IOException {
-        // alle Parameternamen (keys)
-        Enumeration<String> paramNames = request.getParameterNames();
-
-        String responseStr = "";
-        String param = "";
-        while (paramNames.hasMoreElements()) {
-            param = paramNames.nextElement();
-            responseStr = responseStr + param + "="
-                    + request.getParameter(param) + "\n";
-        }
-        response.setContentType("text/plain");
-        try (PrintWriter out = response.getWriter()) {
-            responseStr += "\n Your request will be sent to ";
-            out.println(responseStr);
-        }
-    }
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        ConnectSystem connectSystem = new ConnectSystem();
+        ObjectMapper mapper = new ObjectMapper();
+
         String body = request.getReader().lines()
                 .reduce("", (acc, a) -> acc + a);
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonInString = body;
-        Song song = mapper.readValue(jsonInString, Song.class);
-        ConnectSystem connectSystem = new ConnectSystem();
-        connectSystem.addSong(song);
-        PrintWriter out = response.getWriter();
-        out.println(body);
+
+        if (body != null) {
+            //String jsonInString = body;
+            try {
+                Song song = mapper.readValue(body, Song.class);
+                if (song.getTitle() != null) {
+                    if (song.getTitle().isEmpty()) {
+                        out.println("Wrong Body");
+                        response.setStatus(400);
+                    } else {
+                        connectSystem.addSong(song);
+                        //out.println(body);
+                        //out.println("http://localhost:8080/songsservlet-PABB/songs?songId=" + song.getId());
+                        response.setStatus(201);
+                        response.setHeader("Location", "/songsservlet-PABB/songs?songId=" + song.getId());
+                    }
+                } else {
+                    out.println("Wrong Body");
+                    response.setStatus(400);
+                }
+            } catch (Exception e) {
+                out.println("Wrong Body");
+                response.setStatus(400);
+            }
+        } else {
+            out.println("No Body");
+            response.setStatus(400);
+        }
     }
 
     @Override
@@ -71,35 +68,46 @@ public class SongServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
-
-        //HashMap<String, String> map = new HashMap<String, String>();
         Enumeration<String> paramNames = request.getParameterNames();
-        /*while (paramNames.hasMoreElements()) {
-            String parameter = paramNames.nextElement();
-
-            map.put(parameter, request.getHeader(parameter));
-            //out.println("Parameter  " + parameter);
-            //out.println("Value  " + request.getParameter(parameter));
-        }*/
+        Enumeration<String> paramNames2 = request.getParameterNames();
         ConnectSystem connectSystem = new ConnectSystem();
-        String paramType = paramNames.nextElement();
-        if (paramType.equals("songId")) {
-            int id = Integer.parseInt(request.getParameter(paramType));
-            Song song = connectSystem.getSong(id);
-
-            ObjectMapper mapper = new ObjectMapper();
-            //String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(song);
-            out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(song) + "    " + paramType);
-        } else if(paramType.equals("all")) {
-            List<Song> songList = connectSystem.getSongs();
-            ObjectMapper mapper = new ObjectMapper();
-            out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(songList));
+        int counter = 0;
+        // paramNames.asIterator().forEachRemaining(c -> c, counter+1);
+        while (paramNames2.hasMoreElements()) {
+            paramNames2.nextElement();
+            counter++;
         }
-
+        if (counter == 1) {
+            String paramType = paramNames.nextElement();
+            if (paramType.equals("songId")) {
+                if (request.getParameter(paramType).matches("-?\\d+")) {
+                    int id = Integer.parseInt(request.getParameter(paramType));
+                    Song song = connectSystem.getSong(id);
+                    if (song != null) {
+                        ObjectMapper mapper = new ObjectMapper();
+                        out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(song));
+                        response.setStatus(200);
+                    } else {
+                        out.println("No Song with this ID");
+                        response.setStatus(404);
+                    }
+                } else {
+                    response.setStatus(400);
+                    out.println("songId must be a number");
+                }
+            } else if (paramType.equals("all")) {
+                List<Song> songList = connectSystem.getSongs();
+                ObjectMapper mapper = new ObjectMapper();
+                out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(songList));
+                response.setStatus(200);
+            } else {
+                response.setStatus(400);
+                out.println("wrong parameters");
+            }
+        } else {
+            response.setStatus(400);
+            out.println("too many parameters");
+        }
     }
 
-    @Override
-    public void destroy() {
-
-    }
 }
